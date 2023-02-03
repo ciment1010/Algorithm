160a4733330f6378293668e9aecf98f7ee685ce5
ciment1010 <ciment1010@gmail.com>
2023-02-03 16:31:20 +0900

    Prefix sum

---
+++ 07_Dynamic/!BOJ11659.cpp
@@ -0,0 +1,23 @@
+/*
+Prefix sum
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, d[100005];
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    d[0] = 0;
+    for(int i = 1; i <= n; i++){
+        int num; cin >> num;
+        d[i] = d[i-1] + num;
+    }
+    while(m--){
+        int a, b; cin >> a >> b;
+        cout << d[b] - d[a-1] << '\n';
+    }
+}
